9f532b40e7715643615955d41d7e036afa6493f3
Zunaid <ja7667924@gmail.com>
2026-04-26 14:06:57 +0530

    optimized Input Field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,16 +1,21 @@
-interface Props {
+import type {UseFormRegisterReturn} from "react-hook-form";
+import type {InputHTMLAttributes} from "react";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
     name: string,
+    register: UseFormRegisterReturn,
+    error: string,
     label?: string;
-    placeholder?: string;
+    type?: "text" | "password";
 }
 
-function InputField({name, label, placeholder}: Props) {
+function InputField({name, register, error, label, type = "text"}: Props) {
     return (
         <div>
             {label && (<span className="text-gray-100 text-sm font-semibold">{label}</span>)}
-            <input name={name} placeholder={placeholder}
+            <input {...register} name={name} type={type}
                    className="border border-b-blue-100 text-gray-300 outline-none rounded px-2 py-1.5 w-full"/>
-            <small className="text-red-700"></small>
+            {error && <small className="text-red-700">error</small>}
         </div>
     );
 }
