b8bca1d8144c3c5635fd63bce2aa311773c46743
Zunaid <ja7667924@gmail.com>
2026-04-27 15:42:46 +0530

    removed test bg colors

--- src/components/UserCon.tsx
+++ src/components/UserCon.tsx
@@ -5,9 +5,9 @@ function UserCon() {
         <div
             className="w-full max-w-lg h-13 flex items-center bg-prussianblue border  gap-2 rounded-lg text-white px-3">
             <ProfilePic src={undefined} latter="A"/>
-            <div className="bg-gray-950 flex flex-col">
-                <h2 className="text-sm bg-blue-600 font-bold">John</h2>
-                <small className="bg-red-400">Online</small>
+            <div className="flex flex-col">
+                <h2 className="text-sm font-bold">John</h2>
+                <small className="">Online</small>
             </div>
         </div>
     );
