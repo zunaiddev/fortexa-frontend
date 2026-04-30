72764942f93d471e17f14d1f7145c40f5f445b83
Zunaid <ja7667924@gmail.com>
2026-04-30 16:03:10 +0530

    changes styles of input

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -13,7 +13,7 @@ function SearchInput({...props}: Props) {
                     <SearchIcon/>
                 </div>
                 <input type="search" id="search"
-                       className="block w-full p-3 ps-9 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body"
+                       className="block w-full p-3 ps-9 bg-prussianblue border text-white text-sm rounded"
                        placeholder="Search" required/>
                 <button type="button"
                         className="absolute inset-e-1.5 bottom-1.5 text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded text-xs px-3 py-1.5 focus:outline-none">Search
