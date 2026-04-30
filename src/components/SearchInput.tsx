d93011ea74f0fb3deed7629fab9cada815f6c5c2
Zunaid <ja7667924@gmail.com>
2026-04-30 16:00:49 +0530

    minor changes

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,5 @@
 import type {InputHTMLAttributes} from "react";
+import SearchIcon from "../assets/SearchIcon.tsx";
 
 type Props = InputHTMLAttributes<HTMLInputElement>
 
@@ -9,11 +10,7 @@ function SearchInput({...props}: Props) {
             <label htmlFor="search" className="block mb-2.5 text-sm font-medium text-heading sr-only ">Search</label>
             <div className="relative">
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                    <svg className="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                         height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
-                              d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
-                    </svg>
+                    <SearchIcon/>
                 </div>
                 <input type="search" id="search"
                        className="block w-full p-3 ps-9 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body"
